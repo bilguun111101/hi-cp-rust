e696003c55b3f676dd60a3f523b2739cae373840
bilguun111101 <102782221+bilguun111101@users.noreply.github.com>
2026-09-16 19:13:42 +0800

    feat(cses): 'cses/introductory/gray_code' solved

--- src/cses/introductory/gray_code.rs
+++ src/cses/introductory/gray_code.rs
@@ -1,12 +1,21 @@
 use std::io::{self, Read};
 
-fn main() {
+pub fn gray_code() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let n = input.trim().parse::<usize>().unwrap();
-    let bit_n = 1u64 << n;
-
-    for i in 0..bit_n {
+    for i in 0..1u64 << n {
         println!("{:0width$b}", i ^ (i >> 1), width = n);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gray_code() {
+        gray_code();
+        assert_eq!(0, 0);
     }
 }
